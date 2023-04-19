4f7b29e1c29e6514278401e940ede37b5b891544
Rati3010 <mratikanta1997@gmail.com>
2023-04-19 20:44:44 +0530

    added the final

--- Techlegendary/index.js
+++ Techlegendary/index.js
@@ -3,41 +3,53 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connection } from "./config/db.js";
 import { Quiz } from "./models/QuizModel.js";
+import quizRouter from "./router/quizzRouter.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin:"*"
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to Live Quizz");
+function updateQuizStatus() {
+  Quiz.find({})
+    .then((quizzes) => {
+      quizzes.forEach(async (quiz) => {
+        var now = new Date();
+        if (now < quiz.startDate) {
+          quiz.status = "inactive";
+        } else if (now >= quiz.startDate && now <= quiz.endDate) {
+          quiz.status = "active";
+        } else {
+          quiz.status = "finished";
+        }
+        await quiz.save();
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+app.use((req, res, next) => {
+  updateQuizStatus();
+  next();
 });
-app.post("/quizzes",async (req,res)=>{
-     const quizz = req.body;
-     try {
-        const newQuizz = new Quiz(quizz)
-        await newQuizz.save()
-        res.json({message:"Created a Quizz"});
-     } catch (error) {
-        res.json({error:"Facing Problem during Post. Try after some time..."})
-     }
-})
-app.get("/quizzes/active",(req,res)=>{
 
-})
-app.get("/quizzes/:id/result",(req,res)=>{
+app.get("/", (req, res) => {
+  res.send("Welcome to Live Quizz");
+});
 
-})
-app.get("/quizzes/all",(req,res)=>{
+app.use("/quizzes",quizRouter)
 
-})
-app.listen(process.env.PORT,async()=>{
-     try {
-        await connection;
-        console.log("Connected to DB port:-",process.env.port);
-     } catch (error) {
-        console.log(error);
-     }
-})
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB port:-", process.env.port);
+  } catch (error) {
+    console.log(error);
+  }
+});
